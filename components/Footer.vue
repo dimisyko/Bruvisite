ea6e7f1051ffdc4069ee3f2bdf8401808e1e27ab
dimisyko <dimitri.syko@hotmail.com>
2022-12-15 22:42:40 +0100

    update

--- components/Footer.vue
+++ components/Footer.vue
@@ -77,7 +77,7 @@
       <div class="footer__credits">
         <div class="footer__credits-wrapper d-flex-between w-90">
         <p class="footer__privacy txt-primary">© {{GetYear}} - Tout droit réservé</p>
-        <p class="txt-primary"> Désigné et codé par Sykovaris Dimitrios</p>
+        <p class="txt-primary"> Désigné et codé par <a class="folio-dev" href="https://dimi-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer">Sykovaris Dimitrios</a></p>
         </div>
       </div>
   </footer>
@@ -212,6 +212,10 @@ export default {
   padding: 30px 0;
   border-top: 1px solid white;
   color: white;
+}
+.folio-dev{
+  color: white;
+  text-decoration: underline;
 }
 /* responsive */
 @media (max-width: 1024px){
